1f11d92548c308a39ec4bb9ee43d098843428351
Sanjay Mali <malisanjay5433@gmail.com>
2022-12-16 18:36:49 +0530

    Chore : Login UI setup and Karoo local json file added

--- KaroooooCodeChallenge/ViewController.swift
+++ KaroooooCodeChallenge/ViewController.swift
@@ -10,23 +10,6 @@ class ViewController: UIViewController {
     var users = [Usermodel]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        DispatchQueue.main.async {
-            let localJSON = UserViewModel.init().readJSONFromLocalFile(fileName: "Karoo", type: [Usermodel].self)
-            print("localJSON : \(String(describing: localJSON))")
-        }
     }
-//    func loadJson(filename fileName: String) -> [Usermodel]? {
-//        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
-//            do {
-//                let data = try Data(contentsOf: url)
-//                let decoder = JSONDecoder()
-//                let jsonData = try decoder.decode([Usermodel].self, from: data)
-//                return jsonData
-//            } catch {
-//                print("error:\(error)")
-//            }
-//        }
-//        return nil
-//    }
 }
 
